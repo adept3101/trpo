f7e977ee3515199ba2996a3853fe3da084ca577b
adept3101 <timofeysitnikov@mail.ru>
2025-09-20 21:10:40 +0300

    main

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from fastapi import FastAPI, HTTPException
 from db_config import SessionLocal, get_db
 from models import Client
 from sqlalchemy.future import select
+from typing import Optional
 
 app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return "Hello"
 
 
 @app.get("/clients")
@@ -25,10 +21,6 @@ def gets_clients():
 def get_client(id: int):
     db = SessionLocal()
     try:
-        # # client = db.query(Client).where(Client.client_id == id)
-        # client = db.execute(select(Client).where(Client.client_id == id))
-        # res = client.scalars().first()
-        # return client
         res = db.scalar(select(Client).where(Client.client_id == id))
 
         if res is None:
@@ -57,5 +49,55 @@ def add_client(
         db.commit()
         db.refresh(new_client)
         return new_client
+    finally:
+        db.close()
+
+
+@app.delete("/client", summary="Удалить клиента")
+def delete_client(id: int):
+    db = SessionLocal()
+    try:
+        client = db.scalar(select(Client).where(Client.client_id == id))
+        # search = db.execute(select(Client).where(Client.client_id == id))
+        # client = search.scalars().first()
+        if client is None:
+            raise HTTPException(status_code=404, detail="Client not found")
+        db.delete(client)
+        db.commit()
+    finally:
+        db.close()
+
+
+@app.put("/client", summary="Обновить клиента")
+def update_client(
+    id: int,
+    name: str,
+    lastname: str,
+    phone: str,
+    email: str,
+    registr_data: str,
+):
+    db = SessionLocal()
+    try:
+        # client = db.scalar(select(Client).where(Client.client_id == id))
+        # res = db.execute(select(Client).where(Client.client_id == id))
+        # client = res.scalar().first()
+        client = db.query(Client).filter(Client.client_id == id).first()
+        if client is None:
+            raise HTTPException(status_code=404, detail="Client not found")
+
+        if name is not None:
+            client.name = name
+        if lastname:
+            client.lastname = lastname
+        if phone:
+            client.phone = phone
+        if email:
+            client.email = email
+        if registr_data:
+            client.registr_data = registr_data
+
+        db.commit()
+        db.refresh(client)
     finally:
         db.close()

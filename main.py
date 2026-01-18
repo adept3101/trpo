7f5edaf938be61a926aae3942dbfaa2e39539fb3
adept3101 <timofeysitnikov@mail.ru>
2026-01-18 16:02:34 +0300

    upd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from api import clients, employee, products
 from auth import auth
@@ -8,9 +8,14 @@ from auth import auth
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-def nav_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
+@app.get("/")
+def root():
+    return RedirectResponse("/auth/login")
+
+@app.get("/nav", response_class=HTMLResponse)
+def nav(request: Request):
+    return templates.TemplateResponse("nav.html", {"request": request})
+
 
 app.add_middleware(
     CORSMiddleware,
